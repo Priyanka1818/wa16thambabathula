88daa998650d770067dd5f65f6536b058e2db8fc
Priyanka1818 <priyankathambabathula1998@gmail.com>
2021-03-24 20:04:16 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Priyanka Thambabathula' });
+  res.render('mydata', {title: 'Priyanka Thambabathula'});
 });
 
 module.exports = router;
